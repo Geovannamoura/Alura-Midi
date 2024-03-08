a3d523a8d14606369b87c6c3150187ca135ef3bf
Geovanna Moura <82971092+Geovannamoura@users.noreply.github.com>
2024-03-08 16:54:43 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+function tocaSom (idElementAudio) {
+    document.querySelector(idElementAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function() {
+        tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'space')
+
+        if (evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+
+}
